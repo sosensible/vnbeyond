69f0c681de5b8bfa1e4cb6e57dc4501c99e4d84c
sosensible <johnfarrar@sosapps.com>
2023-10-20 16:45:57 -0400

    Verify Git function before adding content

--- pages/index.vue
+++ pages/index.vue
@@ -2,5 +2,8 @@
   <div>
     <h1>Learning Creators Hub</h1>
     <div>Stuff and more</div>
+    <NuxtLink to="/about">About</NuxtLink>
+    <NuxtLink to="/content">Content</NuxtLink>
+
   </div>
 </template>
